b9ad2a960baee08c58431ffe81ec6979841558ff
lgzzk <3088773042@qq.com>
2023-07-16 12:11:56 +0800

    优化统一返回体

--- huike-admin/src/main/java/site/lgzzk/common/CaptchaController.java
+++ huike-admin/src/main/java/site/lgzzk/common/CaptchaController.java
@@ -2,7 +2,6 @@ package site.lgzzk.common;
 
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.LineCaptcha;
-import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.IdUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.lgzzk.core.domain.Result;
 
-import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 import static site.lgzzk.constant.RedisConstants.CAPTCHA_KEY;
@@ -31,9 +29,7 @@ public class CaptchaController {
         String captchaCode = captcha.getCode();
         redisTemplate.opsForValue().set(captcha_key, captchaCode, CAPTCHA_TTL, TimeUnit.MINUTES);
         String base64 = Base64Utils.encodeToString(captcha.getImageBytes());
-        Result result = Result.ok();
-        result.put("uuid", uuid);
-        result.put("img", base64);
-        return result;
+        return Result.ok()
+                     .put("uuid", uuid).put("img", base64);
     }
 }
--- huike-common/src/main/java/site/lgzzk/core/domain/Result.java
+++ huike-common/src/main/java/site/lgzzk/core/domain/Result.java
@@ -48,4 +48,9 @@ public class Result extends HashMap<String, Object> {
         return new Result(ERROR, msg, null);
     }
 
+    @Override
+    public Result put(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
 }
